d036f6288e2cae575e7d419b419c6c752e97471d
Moisés <moisesnetored@gmail.com>
2022-05-31 10:24:38 -0400

    feat: Controller, Service and authentication route created, axios added.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,21 @@
+import "dotenv/config";
 import express from 'express';
+
+import {router} from './routes';
 
 const app = express();
 
-app.listen(4000, () => console.log("Server is running! PORT 4000"))
+app.use(express.json());
+app.use(router);
+
+app.get("/github", (request, response) => {
+  response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+})
+
+app.get("/signin/callback", (request, response) => {
+  const { code } = request.query;
+
+  return response.json(code);
+})
+
+app.listen(process.env.PORT || 4000, () => console.log(`Server is running! PORT 4000`))
