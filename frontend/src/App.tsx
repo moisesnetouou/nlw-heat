b47faa57ffd3569702cb87607ec49a7af1342045
Moisés <moisesnetored@gmail.com>
2022-06-01 10:35:36 -0400

    fix: spacing import Name component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import styles from './App.module.scss';
-import { LoginBox } from './components/ LoginBox ';
+import { LoginBox } from './components/LoginBox';
 import { MessageList } from './components/MessageList';
 
 export function App() {
