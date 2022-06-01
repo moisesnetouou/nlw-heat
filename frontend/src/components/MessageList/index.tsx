fcb0e124094ca3be421b4d32350710e021ae5692
Moisés <moisesnetored@gmail.com>
2022-06-01 17:15:52 -0400

    feat: Socket.io-client adjusted, messages are displayed dynamically when a new one is added.

--- frontend/src/components/MessageList/index.tsx
+++ frontend/src/components/MessageList/index.tsx
@@ -1,8 +1,9 @@
-import {api} from '../../services/api';
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 
+import {api} from '../../services/api';
 import styles from "./styles.module.scss";
 import logoImg from "../../assets/logo.svg";
-import { useEffect, useState } from 'react';
 
 type Message = {
   id: string;
@@ -13,8 +14,31 @@ type Message = {
   }
 }
 
+const messagesQueue: Message[] = [];
+
+const socket = io('http://localhost:4000');
+
+socket.on('new_message', (newMessage: Message) => {
+  messagesQueue.push(newMessage);
+  console.log(newMessage)
+})
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if(messagesQueue.length > 0) {
+        setMessages(prevState => [
+          messagesQueue[0],
+          prevState[0],
+          prevState[1]
+        ].filter(Boolean))
+
+        messagesQueue.shift()
+      }
+    }, 3000)
+  }, [])
 
   useEffect(()=> {
     api.get<Message[]>('messages/last3').then(response => {
