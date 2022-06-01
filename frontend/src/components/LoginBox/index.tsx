1d18742d52df588543e2c24b22fac85df66c7ba9
Moisés <moisesnetored@gmail.com>
2022-06-01 15:03:07 -0400

    env: Environment variables adjusted.

--- frontend/src/components/LoginBox/index.tsx
+++ frontend/src/components/LoginBox/index.tsx
@@ -1,12 +1,13 @@
-
 import styles from "./styles.module.scss";
 import { VscGithubInverted } from "react-icons/vsc";
 
 export function LoginBox() {
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=${import.meta.env.VITE_GITHUB_CLIENT_ID}`
+
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href="#" className={styles.signInWithGithub}>
+      <a href={signInUrl} className={styles.signInWithGithub}>
         <VscGithubInverted size="24" />
         Entrar com Github
       </a>
